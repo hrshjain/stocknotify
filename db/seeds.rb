6e7cfafefd711519221db16ed3fcfe6c5c4bf35b
Harsh Jain <harshagrawaljain@gmail.com>
2018-08-16 00:55:07 -0700

    letter opener for prod

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-if Rails.env.development?
+# if Rails.env.development?
   AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
   Notification.create!(stock_symbol: 'FB', lower_limit: 180, upper_limit: 190, enable_email_notification: true,\
                         enable_sms_notification: true, admin_user_id: 1)
@@ -18,4 +18,4 @@ if Rails.env.development?
                         enable_sms_notification: true, admin_user_id: 2)
   Notification.create!(stock_symbol: 'AAPL', lower_limit: 300, upper_limit: 400, enable_email_notification: true,\
                         enable_sms_notification: true, admin_user_id: 2)
-end
+# end
